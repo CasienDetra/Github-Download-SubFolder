0c6d76ba9fd765cc9be08e6f4092343c506bc39c
CasienDetra <sarath.rithyyanouk.1223@rupp.edu.kh>
2026-02-22 14:57:45 +0700

    feat : parser added

--- internal/github/parser.go
+++ internal/github/parser.go
@@ -9,10 +9,10 @@ import (
 func ParseLink(link string) (*RepoInfo, error) {
 	link = strings.TrimSuffix(link, "/")
 
-	re := regexp.MustCompile(`^https://github\.com/([^/]+)/([^/]+)(?:/tree/([^/]+))?(?:/(.+))?$`)
+	re := regexp.MustCompile(`^https://github\.com/([^/]+)/([^/]+)(?:(?:/tree/|/blob/)([^/]+))?(?:/(.+))?$`)
 	matches := re.FindStringSubmatch(link)
 	if len(matches) != 5 {
-		return nil, fmt.Errorf("invalid Link please try again")
+		return nil, fmt.Errorf("please try link with blob or tree")
 	}
 	return &RepoInfo{
 		Owner:  matches[1],
